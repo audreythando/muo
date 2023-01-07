df6e693684fa4c0ae4113c4d022251258862cde2
AUDREY THANDO MPALUME <audreythando@gmail.com>
2023-01-07 08:41:20 +0200

    refactor: added more images to assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import Mission from './components/Mission/Mission';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <>
+    <Navbar/>
       <Header/>
+      <Mission/>
+      
+   
     </>
   );
 }

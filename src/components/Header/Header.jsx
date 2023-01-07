59b2f0c682dd15e9cf90642d27e97cad1438d164
AUDREY THANDO MPALUME <audreythando@gmail.com>
2023-01-07 08:42:44 +0200

    refactor: fixed styling on Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import './Header.css'
+import Hero from '../../assets/hero.mp4'
 
 const Header = () => {
   return (
-    <div>
-      
-    </div>
+<div className='hero'>
+            <video autoPlay loop muted id='video'>
+                <source src={Hero} type='video/mp4'/>
+            </video>
+            <div className="hero-text">
+                <h1>MUO INVESTMENTS</h1>
+                <h1><span className="blue"> Digitally and  </span>  truly yours</h1>
+
+                <div className="btn-group">
+                    <button className="btn">Learn more</button>
+                    {/* <button className="btn btn-outline">Documentation</button> */}
+                    <button className="btn btn-outline">FAQ</button>
+                </div>
+            </div>
+         
+        </div>
   );
 }
 

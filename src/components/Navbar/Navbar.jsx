bc2545be7c83357dbdf7e31d91c9d8f8dc0159b7
AUDREY THANDO MPALUME <audreythando@gmail.com>
2023-01-07 08:41:53 +0200

    refactor: fixed styling on Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react'
+import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
+import Logo from '../../assets/Logo.png'
+import './Navbar.css'
 
 const Navbar = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+    const [click, setClick] = useState(false)
+    const handleClick = () => setClick(!click)
+
+    return (
+        <div className='navbar'>
+            <div className="container">
+                <img className='nav-img' src={Logo} alt='nav-logo' /> 
+
+                <ul className={click ? 'nav active' : 'nav'}>
+                    <li className="nav-item">
+                        <a href="/">Platform</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="/">Developers</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="/">Community</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="/">About</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className='btn' href="/">Use Defi</a>
+                    </li>
+                </ul>
+                <div onClick={handleClick} className="hamburger">
+                    {click ? (<AiOutlineClose className='icon' />) : (<AiOutlineMenu className='icon' />)}
+                    
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default Navbar;
+export default Navbar

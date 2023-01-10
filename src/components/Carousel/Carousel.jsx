9d2879a8fd150ea59681fa794f71293beb611f70
AUDREY THANDO MPALUME <audreythando@gmail.com>
2023-01-10 07:06:23 +0200

    refactor: installed React-slick

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,28 +1,136 @@
-import React from 'react';
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
-import Card from './CarouselCard';
+import React , {useState} from 'react'
+import Slider from 'react-slick'
 import './Carousel.css'
-import { BlogPosts, responsive } from '../../data';
+import { BsArrowLeft, BsArrowRight } from 'react-icons/bs';
+import img1 from '../../assets/BLOG.jpg';
+import img2 from '../../assets/blogSeven.jpg';
+import img3 from '../../assets/blogThree.jpg';
+import img4 from '../../assets/blogFour.jpg';
+import img5 from '../../assets/blogSix.jpg';
 
-const CarouselBlog = () => {
 
-    const blog = BlogPosts.map((item) => (
-        <Card
-          name={item.name}
-          url={item.imageurl}
-          description={item.description}
-        />
-      ));
-    
-  return (
-    <div className='blog-posts'>
-    <h1>Latest Articles</h1>
-    <Carousel showDots={true} responsive={responsive}>
-      {blog}
-    </Carousel>
+
+
+const images =[{
+  id: 1,
+  img:img1,
+
+  description: "Some text about the product..",
+},
+{
+  id: 2,
+  img:img2,
+  description: "Some text about the product..",
+},
+{
+  id: 2,
+  img:img3,
+  description: "Some text about the product..",
+},
+{
+  id: 2,
+  img:img3,
+
+  description: "Some text about the product..",
+},
+{
+  id: 2,
+  img:img4,
+  description: "Some text about the product..",
+},
+{
+  id: 2,
+  img:img5,
+  description: "Some text about the product..",
+},
+
+]
+
+function SampleNextArrow( onClick ) {
+return (
+  <div className='arrow arrow-right' onClick={onClick}>
+   < BsArrowRight/>
   </div>
-  );
+ 
+  
+);
 }
 
-export default CarouselBlog;
+function SamplePrevArrow( onClick ) {
+return (
+  
+  <div className='arrow arrow-left' onClick={onClick}>
+    <BsArrowLeft/>
+  </div>
+  
+);
+}
+
+
+const Carousel = () => {
+  const [slideIndex , setSlideIndex]=useState()
+
+  const settings ={
+  
+    infinite:true,
+    speed:500,
+    slidesToShow:4,
+    slidesToScroll:1,
+    autoplay:true,
+    autoplaySpeed:1000,
+    beforeChange :( next)=>setSlideIndex(next),
+    centerMode: true,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  }
+
+  return (
+    <div className='container'>
+      <h1 className='header'>Latest Articles  </h1>
+      <div className='slider'>
+<Slider {...settings}  >
+{
+images.map((item,index)=> (
+  <div className={index===slideIndex? 'slide slide-active': 'slide'}
+>
+<img src={item?.img} alt="slider" />
+<h4>{item.description}</h4>
+<button>Read More</button>
+  </div>
+))
+}
+</Slider>
+      </div>
+
+    </div>
+  )
+}
+
+export default Carousel
+
--- src/data.js
+++ src/data.js
@@ -1,11 +1,8 @@
-import BlogOne from './assets/BLOG.jpg'
-import BlogTwo from './assets/blogTwo.png'
-import BlogThree from './assets/blogThree.jpg'
-import BlogFour from './assets/blogFour.jpg'
-import BlogFive from './assets/blogFive.jpg'
-import BlogSix from './assets/blogSix.jpg'
-import BlogSeven from './assets/blogSeven.jpg'
-
+import img1 from './assets/BLOG.jpg';
+import img2 from './assets/blogTwo.png';
+// import img3 from '../../assets/blogThree.jpg';
+// import img4 from '../../assets/blogFour.jpg';
+// import img5 from '../../assets/blogFive.jpg';
 
 export const links = [
     {
@@ -33,68 +30,92 @@ export const links = [
 
 ]
 
+
 export const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 1024 },
-      items: 5,
-      slidesToSlide: 2,
-    },
-    desktop: {
-      breakpoint: { max: 1024, min: 800 },
-      items: 4,
-    },
-    tablet: {
-      breakpoint: { max: 800, min: 464 },
-      items: 2,
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1,
-    },
-  };
-  
-  export const BlogPosts = [
-    {
-      id: 1,
-      imageurl:
-      "https://images.unsplash.com/photo-1560769629-975ec94e6a86?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHByb2R1Y3RzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-      name: "Colorful sneakers",
-      description: "Some text about the product..",
-    },
-    {
-      id: 1,
-      imageUrl: {BlogTwo},
-      description: "Some text about the product..",
-    },
-    {
-      id: 3,
-      imageUrl: {BlogThree},
-      description: "Some text about the product..",
-    },
-    {
-      id: 4,
-      Image: {BlogFour},
-      description: "Some text about the product..",
-    },
-    {
-      id: 5,
-      Image: {BlogFive},
-      description: "Some text about the product..",
-    },
-    {
-      id: 6,
-      Image: {BlogSix},
-      description: "Some text about the product..",
-    },
-    {
-      id: 7,
-      Image: {BlogSeven},
-      description: "Some text about the product..",
-    },
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 1024 },
+    items: 5,
+    slidesToSlide: 2,
+  },
+  desktop: {
+    breakpoint: { max: 1024, min: 800 },
+    items: 4,
+  },
+  tablet: {
+    breakpoint: { max: 800, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
 
-  ];
-
-
-
+export const productData = [
+  {
+    id: 1,
+    imageurl:{img1},
+      // "https://images.unsplash.com/photo-1560769629-975ec94e6a86?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHByb2R1Y3RzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    name: "Colorful sneakers",
+    price: "$19.99",
+    description: "Some text about the product..",
+  },
+  {
+    id: 2,
+    imageurl:{img2},
+      // "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cHJvZHVjdHN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    name: "Sport sneakers",
+    price: "$21.99",
+    description: "Some text about the product..",
+  },
+  {
+    id: 3,
+    imageurl:
+      "https://images.unsplash.com/photo-1546868871-7041f2a55e12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjZ8fHByb2R1Y3RzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    name: "iWatch",
+    price: "$99.99",
+    description: "Some text about the product..",
+  },
+  {
+    id: 4,
+    imageurl:
+      "https://images.unsplash.com/photo-1610824352934-c10d87b700cc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjl8fHByb2R1Y3RzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    name: "Water Bottle",
+    price: "$14.99",
+    description: "Some text about the product..",
+  },
+  {
+    id: 5,
+    imageurl:
+      "https://images.unsplash.com/photo-1525966222134-fcfa99b8ae77?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzB8fHByb2R1Y3RzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    name: "Vans sneakers",
+    price: "$38.99",
+    description: "Some text about the product..",
+  },
+  {
+    id: 6,
+    imageurl:
+      "https://images.unsplash.com/photo-1585386959984-a4155224a1ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fHByb2R1Y3RzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    name: "Coco Noir",
+    price: "$149.99",
+    description: "Some text about the product..",
+  },
+  {
+    id: 7,
+    imageurl:
+      "https://unsplash.com/photos/HOrhCnQsxnQ",
+    name: "Sunglasses",
+    price: "$38.99",
+    description: "Some text about the product..",
+  },
+  {
+    id: 8,
+    imageurl:
+      "https://images.unsplash.com/photo-1625772452859-1c03d5bf1137?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHByb2R1Y3RzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    name: "Dove cream",
+    price: "$49.99",
+    description: "Some text about the product..",
+  },
+];
 

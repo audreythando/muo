239c3cc7569e33c0b5a11634320f1b4565a94288
AUDREY THANDO MPALUME <audreythando@gmail.com>
2023-01-16 01:26:26 +0200

    refactor: fixed styling in Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,18 @@
-import React from 'react';
-import './Header.css'
-import Hero from '../../assets/hero.mp4'
+import React from "react";
+import "./Header.css";
+import Hero from "../../assets/hero.mp4";
 
-const Header = ({title}) => {
+const Header = ({ title }) => {
   return (
-<div className='hero'>
-            <video autoPlay loop muted id='video'>
-                <source src={Hero} type='video/mp4'/>
-            </video>
-            
-    
-       <div>
-       <h1>{title}</h1>
-       </div>
-         
-        </div>
+    <div className="hero">
+      <video autoPlay loop muted id="video">
+        <source src={Hero} type="video/mp4" />
+      </video>
+      <div className="hero-text">
+        <h2>{title}</h2>
+      </div>
+    </div>
   );
-}
+};
 
 export default Header;
--- src/components/HeaderTwo/Header.jsx
+++ src/components/HeaderTwo/Header.jsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import './Header.css'
-import Hero from '../../assets/earth.mp4'
-
+import React from "react";
+import "./Header.css";
+import Hero from "../../assets/earth.mp4";
+import { AiOutlineArrowRight } from "react-icons/ai";
 const Header = () => {
   return (
-<div className='hero'>
-            <video autoPlay loop muted id='video'>
-                <source src={Hero} type='video/mp4'/>
-            </video>
-            <div className="hero-text">
-                <h1>MUO INVESTMENTS</h1>
-                <h2><span className="blue"> Digitally and  </span>  truly yours</h2>
+    <div className="hero">
+      <video autoPlay loop muted id="video">
+        <source src={Hero} type="video/mp4" />
+      </video>
+      <div className="hero-text">
+        <h1>MUO INVESTMENTS</h1>
+        <h2>
+          <span className="blue"> Digitally and </span> truly yours
+        </h2>
 
-                <div className="btn-group">
-                    <button className="btn">Learn more</button>
-                    {/* <button className="btn btn-outline">Documentation</button> */}
-                    <button className="btn btn-outline">FAQ</button>
-                </div>
-            </div>
-         
+        <div className="btn-group">
+          <button className="btn">
+            Learn more <AiOutlineArrowRight />
+          </button>
         </div>
+      </div>
+    </div>
   );
-}
+};
 
 export default Header;
